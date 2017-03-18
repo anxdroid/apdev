28ad4137dea627155d8a4d89b8e3356e92ee118e
Antonello Paoletti <paoletti.antonello@gmail.com>
2017-03-18 20:32:42 +0100

    pulizia

--- SDL2-2.0.5/build/SDL_d3dmath.d
+++
@@ -1,6 +0,0 @@
-build/SDL_d3dmath.lo: /home/pi/apdev/SDL2-2.0.5/src/render/SDL_d3dmath.c \
- /home/pi/apdev/SDL2-2.0.5/src/render/../SDL_internal.h \
- /home/pi/apdev/SDL2-2.0.5/src/render/../dynapi/SDL_dynapi.h \
- /home/pi/apdev/SDL2-2.0.5/src/render/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h
--- SDL2-2.0.5/build/SDL_shaders_gl.d
+++
@@ -1,7 +0,0 @@
-build/SDL_shaders_gl.lo: \
- /home/pi/apdev/SDL2-2.0.5/src/render/opengl/SDL_shaders_gl.c \
- /home/pi/apdev/SDL2-2.0.5/src/render/opengl/../../SDL_internal.h \
- /home/pi/apdev/SDL2-2.0.5/src/render/opengl/../../dynapi/SDL_dynapi.h \
- /home/pi/apdev/SDL2-2.0.5/src/render/opengl/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h
