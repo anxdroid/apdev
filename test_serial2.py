d04f59971cd9fc88f7b3507c8e917e466ec7b2ce
apaolett <Antonello.Paoletti@lnf.infn.it>
2018-02-15 21:12:32 +0100

    serial

--- test_serial2.py
+++ test_serial2.py
@@ -2,15 +2,19 @@ import serial
 from time import sleep, time
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5, 
-        parity=serial.PARITY_ODD,
-        stopbits=serial.STOPBITS_TWO,
-        bytesize=serial.SEVENBITS)
+	bytesize=serial.EIGHTBITS,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	timeout=1,
+	xonxoff=0,
+	rtscts=0)
 
-#while 1:
-#    line = ser.readline()
-#    print(line)
-
-ser.isOpen()
+if(ser.isOpen() == False):
+	ser.open()
+self.serACM.setDTR(False)
+self.serACM.flushInput()
+time.sleep(1)
+self.serACM.setDTR(True)
 
 while 1:
     ser.write("test")
