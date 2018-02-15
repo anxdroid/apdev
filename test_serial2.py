223f7880763973b9c47eb6f7c1196ae5eaf0d9a2
anxdroid <paoletti.antonello@gmail.com>
2018-02-15 19:52:39 +0000

    test seriale

---
+++ test_serial2.py
@@ -0,0 +1,21 @@
+import serial
+from time import sleep, time
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5, 
+        parity=serial.PARITY_ODD,
+        stopbits=serial.STOPBITS_TWO,
+        bytesize=serial.SEVENBITS)
+
+#while 1:
+#    line = ser.readline()
+#    print(line)
+
+ser.isOpen()
+
+while 1:
+    ser.write('test')
+    print("sent..")
+    sleep(1)
+
+ser.close()
+
