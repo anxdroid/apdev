adaa2bc7e8be0836b7b69664c6730a3fe1bba5c4
anxdroid <paoletti.antonello@gmail.com>
2018-02-18 20:16:11 +0000

    update

--- test_i2c.py
+++ test_i2c.py
@@ -1,5 +1,6 @@
 import smbus
 import time
+import urllib, json
 from copy import deepcopy
 
 # for RPI version 1, use "bus = smbus.SMBus(0)"
@@ -11,6 +12,48 @@ address = 0x04
 CMD_LAST_CHUNK = 0x00
 CMD_INTERMEDIATE_CHUNK = 0x01
 CMD_GET_TOUCH_CMD = 0x10
+
+API = ["", "", "", "sendSolarData", "", "", "", "", "", "", "sendTempData"]
+
+APIKEY = "a7441c2c34fc80b6667fdb1717d1606f"
+class APIs:
+    def sendSolarData(self, apiId):
+        #var = '{"apiId":'+str(apiId)+', "values":["324"], "timestamps":[1112233212]}'
+        url = "http://192.168.1.12/emoncms/feed/data.json?id=22&start=1518780000000&end=1518799900000&interval=1800&skipmissing=0&limitinterval=1&apikey="+APIKEY
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+        payload = "[";
+        index = 0;
+        for m in data :
+            if (m[1] is not None) :
+                #print m
+                if (index > 0) :
+                    payload += ", "
+                payload += '["'+str(int(m[0])/1000)+'", "'+str(m[1])+'"]'
+                index += 1;
+        payload += "]"
+
+        #print payload;
+        
+        
+        var = '''{"i":'''+str(apiId)+''', "p":[
+        {"t":"1518974662", "v":"700.23"},
+        {"t":"1518974562", "v":"700.30"},
+        {"t":"1518974462", "v":"700.40"},
+        {"t":"1518974362", "v":"700.44"},
+        {"t":"1518974262", "v":"700.45"},
+        {"t":"1518974162", "v":"700.50"}]}'''
+        
+        var = '{"i":'+str(apiId)+', "p":'+payload+'}'
+
+        sendString(var)
+    def sendTempData(self, apiId):
+        #var = '{"apiId":'+str(apiId)+', "values":["324"], "timestamps":[1112233212]}'
+        var = '''{"i":'''+str(apiId)+''', "p":[
+        {"t":"1518974662", "v":"19.23"},
+        {"t":"1518974600", "v":"19.50"}
+        ]}'''
+        sendString(var)
 
 def sendString(value):
     try :
@@ -24,30 +67,31 @@ def sendString(value):
             if (nchunk == (len(chunks) - 1)) :
                 # last chunk
                 cmd = CMD_LAST_CHUNK
-            #print str(nchunk)+" of "+str(len(chunks))+": "+str(chunk)
             bus.write_i2c_block_data(address, cmd, chunk)
-            time.sleep(0.5)
-            #resp = bus.read_i2c_block_data(address, 0)
-            #number = readNumber()
-            #print "Sent "+str(len(chunk))+" received "+str(number)
+            time.sleep(0.1)
             nchunk += 1
         time.sleep(1)
         print "Waiting for response..."
         resp = bus.read_i2c_block_data(address, 0)
         while (resp is None) :
             resp = bus.read_i2c_block_data(address, 0)
-        print "Got response: "+str(resp)+" = "+BytesToString(resp)
+        print "Got "+BytesToString(resp)+" bytes, sent "+value+" ["+str(len(value))+"]"
     except IOError as e:
         return 0
 
 def sendCmd(cmd):
     try :
-        #resp = bus.read_i2c_block_data(address, cmd)
-        #resp = bus.read_byte_data(address, cmd)
-        resp = bus.read_i2c_block_data(address, cmd, 1)
-        return resp
+        resp = bus.read_i2c_block_data(address, cmd, 4)
+        if resp is not None and isinstance(resp, list) and len(resp) > 0 and resp[0] != 48: 
+            resp = int(BytesToString(resp))
+            if resp > 0:
+                return resp
+
     except IOError as e:
         return 0
+    except ValueError as e:
+        return 0
+    return 0
 
 def BytesToString(val):
     retVal = ''
@@ -75,18 +119,20 @@ def StringToBytes(val):
         #print str(len(chunk))+" bytes"
         retVal.append(deepcopy(chunk))
     
-    print "Found "+str(len(retVal))+" chunks"
+    #print "Found "+str(len(retVal))+" chunks"
     return retVal
 
 while True:
+    api = APIs()
     #var = '{"cmd":"getTouchCmd", "idCmdMaster":"12", "idCmdSlave":"0", "status":"0", "response":""}'
     #sendString(var)
     resp = sendCmd(CMD_GET_TOUCH_CMD)
-    #print str(type(resp))
-    if resp is not None and isinstance(resp, list) and len(resp) > 0 : 
-        resp = int(chr(int(resp[0])))
     if (resp > 0) :
         print str(resp)
+        print API[resp]
+        if (API[resp] != "") :
+            time.sleep(0.1)
+            getattr(api, API[resp])(resp)
     #time.sleep(0.1)
 
     #number = readNumber()
