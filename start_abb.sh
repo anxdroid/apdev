3ab3e8b0697b37667d7f60c88bc8809c6773490f
anxdroid <paoletti.antonello@gmail.com>
2018-02-26 00:39:44 +0100

    termostato

---
+++ start_abb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+	do
+		echo "Start..."
+		python /home/pi/apdev/test_abb.py
+		echo "End..."
+	done
---
+++ test_termostato.py
@@ -0,0 +1,94 @@
+import time
+import urllib2
+import json
+import datetime
+import calendar
+
+class Termostato:
+    APIKEY = "a7441c2c34fc80b6667fdb1717d1606f"
+    urlCurrTemp = "http://192.168.1.12/emoncms/feed/timevalue.json"
+    urlHeaters = "http://192.168.1.12/temp/jobs.php"
+    heatersMgrUsr = "anto"
+    heatersMgrPwd = "resistore"
+    minTemp = 17
+    maxTemp = 20
+    interval = 60
+
+    def __init__(self, minTemp, maxTemp, interval):
+        self.minTemp = minTemp
+        self.maxTemp = maxTemp
+        self.interval = interval
+
+    def startTermo(self) :
+        while True:
+            temp = self.getCurrTemp()
+            #print str(temp)
+            now = time.time()
+            tempVal = float(temp["value"])
+            tempTime = int(temp["time"])
+            diffTempTime = now - tempTime
+            status = self.getHeatersStatus()
+            cmd = status["cmd"]
+            cmdStatus = int(status["status"])
+            cmdEnded = status["ended"]
+            date = datetime.datetime.strptime(cmdEnded, "%Y-%m-%d %H:%M:%S")
+            #cmdTime = calendar.timegm(date.utctimetuple())
+            cmdTime = time.mktime(date.timetuple())
+            diffCmdTime = now - cmdTime
+            print str(time.time())+": Temp "+str(diffTempTime)+" secs ago was "+str(tempVal)+" ("+str(self.minTemp)+"-"+str(self.maxTemp)+")"
+            print str(time.time())+": Heaters "+str(diffCmdTime)+" secs ago where "+str(cmd)+" ("+str(cmdStatus)+")"
+            if diffTempTime > 120 :
+                print "No decision taken !"
+            else:
+                if ((temp < self.minTemp) and (cmd != "HEATERS:ON" or cmdStatus != 2)) :
+                    print "Start heaters !"
+                    self.toggleHeatersStatus("HEATERS:ON")
+                if ((temp > self.maxTemp) and (cmd != "HEATERS:OFF" or cmdStatus != 2)) :
+                    print "Stop heaters !"
+                    self.toggleHeatersStatus("HEATERS:OFF")
+            
+            time.sleep(self.interval)
+
+    def getCurrTemp(self):
+        url = self.urlCurrTemp+"?id=10&apikey="+self.APIKEY
+        #print url
+        response = urllib2.urlopen(url)
+        data = json.loads(response.read())
+        return data
+    
+    def toggleHeatersStatus(self, switch):
+        url = self.urlHeaters+"?cmd="+switch
+        print url
+        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        password_mgr.add_password(None, url, self.heatersMgrUsr, self.heatersMgrPwd)
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+        opener.open(url)
+        urllib2.install_opener(opener)
+        response = urllib2.urlopen(url)
+        data = json.loads(response.read())
+        return data["data"][0]
+
+    def getHeatersStatus(self):
+        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        password_mgr.add_password(None, self.urlHeaters, self.heatersMgrUsr, self.heatersMgrPwd)
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+        opener.open(self.urlHeaters)
+        urllib2.install_opener(opener)
+        response = urllib2.urlopen(self.urlHeaters)
+        data = json.loads(response.read())
+        return data["data"][0]
+
+
+
+def main():
+    termo = Termostato(20, 22, 10)
+    #status = termo.getHeatersStatus()
+    #temp = termo.getCurrTemp()
+    #termo.toggleHeatersStatus("ON")
+    #termo.toggleHeatersStatus("OFF")
+    termo.startTermo()
+
+
+if __name__ == "__main__": main()
