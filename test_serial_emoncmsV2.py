64e377be7bd4ed62bd2175f20f5e030d4acafab1
apaolett <Antonello.Paoletti@lnf.infn.it>
2018-03-29 21:27:33 +0200

    serial

--- test_serial_emoncmsV2.py
+++ test_serial_emoncmsV2.py
@@ -183,7 +183,8 @@ class APServer(object):
 			if(self.serACM.isOpen() == False):
 				self.serACM.open()
 			print('Writing cmd to serial...')
-			self.serACM.write(cmd+'\r'.encode())
+			cmd += '\r'
+			self.serACM.write(cmd.encode())
 			#if (self.serACM.inWaiting() > 0):
 			#	myline = self.serACM.readline()
 			#	self.serACM.flushInput()
