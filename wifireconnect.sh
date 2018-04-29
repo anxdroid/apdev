55aae274b42cd8155461b8359032b464ce213469
apaolett <Antonello.Paoletti@lnf.infn.it>
2018-04-29 11:58:51 +0200

    wifi reconect

---
+++ wifireconnect.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Reconnection script started."
+sleep 10
+
+while true
+do
+	if ! ifconfig | grep --quiet "192.168.2."; then
+	echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Not connected, checking if LAN3 network is available..."
+	if iwlist wlan0 scan | grep --quiet "LAN3"; then
+		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Network is available! Restarting WLAN..."
+		ifconfig wlan0 down > /dev/null
+		sleep 1
+		ifconfig wlan0 up > /dev/null
+		sleep 10
+		if ifconfig | grep --quiet "192.168.2."; then
+		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Reconnection successful!"
+		else
+		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Reconnection failed!"
+		fi
+	else
+		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Network is not available... going to sleep for 20s."
+		sleep 20
+	fi
+	fi
+	sleep 10
+done
