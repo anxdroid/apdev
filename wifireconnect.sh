188ea1c37c7c2a07023002362591536f0fa8d503
anxdroid <paoletti.antonello@gmail.com>
2018-04-29 12:06:46 +0200

    wifi reconnect

--- wifireconnect.sh
+++ wifireconnect.sh
@@ -6,23 +6,22 @@ sleep 10
 
 while true
 do
-	if ! ifconfig | grep --quiet "192.168.2."; then
-	echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Not connected, checking if LAN3 network is available..."
-	if iwlist wlan0 scan | grep --quiet "LAN3"; then
-		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Network is available! Restarting WLAN..."
+	if ! ifconfig | grep --quiet "192.168.1."; then
+		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Not connected..."
+		#if iwlist wlan0 scan | grep --quiet "LAN3"; then
+		#echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Network is available! Restarting WLAN..."
 		ifconfig wlan0 down > /dev/null
 		sleep 1
 		ifconfig wlan0 up > /dev/null
 		sleep 10
-		if ifconfig | grep --quiet "192.168.2."; then
-		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Reconnection successful!"
+		if ifconfig | grep --quiet "192.168.1."; then
+			echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Reconnection successful!"
 		else
-		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Reconnection failed!"
+			echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Reconnection failed!"
 		fi
 	else
-		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Network is not available... going to sleep for 20s."
+		echo `date "+%Y-%m-%d %H:%M:%S" | tr -d '\n'` "Connected... going to sleep for 20s."
 		sleep 20
-	fi
 	fi
 	sleep 10
 done
